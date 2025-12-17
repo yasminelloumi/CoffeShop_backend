d62cf9172420bb8529e5cf995ccec1b81b77098c
ZainebAffes <zaineb.affes20@gmail.com>
2025-12-17 20:18:52 +0100

    ConfigureCors

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const path = require('path');
 const app = express();
 
 app.use(express.json());
+const cors = require('cors');
+app.use(cors());
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 // Routes
 app.use("/api/products", require("./routes/products.routes"));
